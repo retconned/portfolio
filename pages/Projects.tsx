d3e26fe3d8c999cba7751a80e86c0d576b864604
retconned <retcnned@gmail.com>
2022-04-25 02:50:59 +0200

    Moved components folder

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -1,26 +1,24 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import Project from "./Components/Project";
-import FeaturedProject from "./Components/FeaturedProject";
-import Nav from "./Components/Nav";
+import Project from "../Components/Project";
+import FeaturedProject from "../Components/FeaturedProject";
+import Nav from "../Components/Nav";
 
 const Projects: NextPage = () => {
   return (
-    <div className="h-screen bg-black">
-      {/* <div className=" flex flex-row divide-x-2 divide-red-600">
-        <Nav />
-      </div> */}
+    <div className="flex h-screen bg-black">
       <Nav />
-
-      {/* <div className=" flex flex-col space-y-4 bg-red-500">
-        <Project />
-        <Project />
-      </div> */}
-      {/* <div className="flex w-full flex-row flex-wrap justify-around bg-red-300 p-2 ">
-        <FeaturedProject />
-        <FeaturedProject />
-        <FeaturedProject />
-      </div> */}
+      <div>
+        <div className=" flex flex-col space-y-4 bg-red-500">
+          <Project />
+          <Project />
+        </div>
+        <div className="flex w-full flex-row flex-wrap justify-around bg-red-300 p-2 ">
+          <FeaturedProject />
+          <FeaturedProject />
+          <FeaturedProject />
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from "next";
 import Image from "next/image";
-import Nav from "./Components/Nav";
+import Nav from "../Components/Nav";
 
 const Home: NextPage = () => {
   return <div className="h-screen bg-stone-900">{/* <Nav /> */}</div>;
