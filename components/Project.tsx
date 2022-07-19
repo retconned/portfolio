b6366665a7b1da13f1254739a8d87fbfef8313ef
retconned <retcnned@gmail.com>
2022-07-19 15:06:59 +0100

    Updated Project component

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,27 +1,46 @@
-import type { NextPage, NextComponentType } from "next";
-import Image from "next/image";
-interface ProjectComponentProps {
-  title: string;
-  tech: string;
-  description: string;
-}
+import type { NextPage } from "next";
+
 const Project: NextPage<ProjectComponentProps> = ({
   title,
   tech,
   description,
-}) => {
+  languages,
+}: any) => {
   return (
-    <div className="">
-      <div className="flex h-24 w-11/12 items-center divide-x divide-stone-900 rounded-md border border-stone-900 bg-black font-mono text-white duration-150 hover:bg-stone-900">
-        <div className="flex h-full w-1/12 min-w-fit flex-col items-center justify-center  px-4">
-          name <span className="text-stone-600">{title}</span>
+    <div
+      onClick={() => {
+        console.log("object");
+      }}
+      className="flex w-80 cursor-pointer flex-col items-center justify-center divide-y divide-stone-900 rounded-md border border-stone-900 bg-black font-mono duration-150 hover:bg-stone-900 group-hover:bg-red-500 md:w-11/12 md:flex-row md:divide-x md:divide-y-0"
+    >
+      <div className="w-full px-4 py-4 text-center text-stone-600  md:flex md:h-24 md:w-[20%] md:flex-col md:items-start md:justify-center md:text-left">
+        <p className="text-white">name</p>
+        {title}
+      </div>
+      <div className="w-full px-4 py-4 text-center text-stone-600  md:flex md:h-24 md:w-2/12 md:min-w-fit md:flex-col md:items-start md:justify-center md:text-left">
+        <p className=" text-white">tech</p>
+        <div className="flex flex-row items-center justify-center space-x-2">
+          <p>{tech}</p>
+          <div>
+            {languages.map((language: any, index: any) => {
+              // console.log(language.name);
+              if (language.name == tech) {
+                return (
+                  <div
+                    key={index}
+                    className={` aspect-square h-2.5 w-2.5 rounded-full`}
+                    style={{ backgroundColor: language.color }}
+                    // className={`aspect-square h-2.5 w-2.5 rounded-full bg-red-300`}
+                  />
+                );
+              }
+            })}
+          </div>
         </div>
-        <div className="flex h-full w-1/12 min-w-fit flex-col items-center justify-center  px-4">
-          tech <span className="text-stone-600">{tech}</span>
-        </div>
-        <div className="flex h-full w-10/12 flex-col items-start justify-center  px-4">
-          <span className="text-stone-600">{description}</span>
-        </div>
+      </div>
+      <div className="w-full px-4 py-4 text-center text-stone-600  md:flex md:h-24 md:flex-col md:items-start md:justify-center md:text-left">
+        <p className="text-white">description</p>
+        {description}
       </div>
     </div>
   );
