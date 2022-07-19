079e344b6501763f2d9775e1e432fc194e10c325
retconned <retcnned@gmail.com>
2022-07-19 13:42:08 +0100

    moved types to d.ts file

---
+++ types.d.ts
@@ -0,0 +1,13 @@
+interface ProjectComponentProps {
+    title: string;
+    tech: string;
+    description: string;
+    languages?: string[];
+}
+interface featuredProjectProps {
+    name?: string;
+    type?: string;
+    iconLink?: string;
+    role?: string;
+    story?: string;
+}
