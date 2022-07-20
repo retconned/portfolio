ab211d3c9398d674e995e838884220d5b213b0dd
retconned <retcnned@gmail.com>
2022-07-20 13:11:55 +0100

    Updated bio & featured project component

--- components/FeaturedProject.tsx
+++ components/FeaturedProject.tsx
@@ -1,13 +1,5 @@
 import type { NextPage } from "next";
-import Image from "next/image";
-
-interface featuredProjectProps {
-  name?: string;
-  type?: string;
-  iconLink?: string;
-  role?: string;
-  story?: string;
-}
+import { ArrowSquareOut } from "phosphor-react";
 
 const FeaturedProject: NextPage<featuredProjectProps> = ({
   name,
@@ -17,26 +9,25 @@ const FeaturedProject: NextPage<featuredProjectProps> = ({
   story,
 }) => {
   return (
-    <div className="flex w-3/12 flex-col divide-y divide-stone-900  rounded-lg bg-black font-mono text-white duration-150 hover:bg-stone-900 ">
-      <div className="group flex h-32 w-96 items-center space-x-4 p-4 ">
-        <div className="flex aspect-square h-24 w-24 flex-grow-0 items-center justify-center rounded-lg bg-blue-500">
-          icon
-        </div>
-        <div className="flex-0 flex h-24 flex-col items-start justify-center ">
-          <div className="flex flex-row space-x-2">
-            <p className="font-bold">{name}</p>
+    <div className="my-4 flex h-fit w-80 cursor-pointer  flex-col items-center justify-between divide-y divide-stone-900 rounded-md border border-stone-900 bg-black font-mono text-white duration-150 hover:bg-stone-900 md:mx-4 md:w-96 md:max-w-3xl md:flex-col md:divide-y">
+      <div className="flex h-[40%] w-full flex-row items-center p-4">
+        <div className="h-16 w-16 rounded-md bg-pink-300" />
+        <div className="px-4">
+          <div className="flex flex-row items-center space-x-1">
+            <p>{name}</p>
+            <ArrowSquareOut size={14} />
           </div>
-          <p className=" text-stone-600">{type}</p>
+          <p className="text-stone-600">{type}</p>
         </div>
       </div>
-      <div className="group flex h-60 flex-col justify-center  p-4 ">
-        <div className="flex flex-col  py-2">
-          <p className="mb-2 font-bold">Role</p>
-          <p className=" text-stone-600"> {role} </p>
+      <div className="h-[60%] min-h-[250px] w-full space-y-4 px-4 py-8 text-stone-600">
+        <div className="">
+          <p className="font-semibold text-white">Role</p>
+          <p>{role}</p>
         </div>
-        <div className="flex flex-col  py-2">
-          <p className="mb-2 font-bold"> Story</p>
-          <p className="text-stone-600 ">{story}</p>
+        <div className="">
+          <p className="font-semibold text-white">What</p>
+          <p className="">{story}</p>
         </div>
       </div>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,54 @@
 import type { NextPage } from "next";
-import Image from "next/image";
+import Head from "next/head";
 import Nav from "../components/Nav";
 
 const Home: NextPage = () => {
-  return <div className="h-screen bg-stone-900">{/* <Nav /> */}</div>;
+  return (
+    <div className="flex flex-row bg-black">
+      <Head>
+        <title>who am i - retconned</title>
+        <meta property="og:title" content="who am i - retconned" key="title" />
+      </Head>
+      <Nav />
+      <div className="flex h-auto min-h-screen bg-black">
+        <div className="w-full bg-black px-8 pt-20 md:pl-8 md:pt-10 ">
+          <article className="prose prose-a:text-pink-500">
+            <h1 className="text-4xl font-bold text-white">who am i 🤔</h1>
+            <p className="w-[55vw] font-medium text-stone-300">
+              <p>
+                Hello, I go by retconned, I am a passionate web developer &
+                designer with 5 years of design experiance and User Interface
+                Engineering. My expertise lies in the design, development, and
+                delivery of astonishing products.
+              </p>
+              <p>
+                I am equipped with a diverse yet a promising skill-set that
+                enables me to effectively self-manage during independent
+                projects as well as collaborate as part of a production team.
+                Utilizing my creative and artistic skills I am proficient in
+                designing and developing interactive and beautiful yet intuitive
+                responsive websites & interfaces.
+              </p>
+              <p>
+                I also have experience creating prototypes Utilizing my creative
+                and artistic skills allow me to create interactive and beautiful
+                yet intuitive responsive websites & interfaces. I also have
+                experience creating prototypes for various web applications.
+              </p>
+              <p>
+                My diverse skillset including my experience as a graphic & a
+                motion design allows me to provide various services, help across
+                multiple departments and work closely with any team across
+                multiple fields. I have experience working on various frameworks
+                and languages including: React.js, Next.js, JavaScript,
+                HTML5/CSS3, TailwindCSS, Typescript, MongoDB and more!
+              </p>
+            </p>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
